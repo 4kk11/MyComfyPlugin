6d21dcb3e9203852fe69983670059db7426fc359
Akihito Yokota <akiaki118row@yahoo.co.jp>
2024-01-27 18:08:05 +0900

    Fix: Minor fix

--- pynode/js/pynode.js
+++ pynode/js/pynode.js
@@ -8,16 +8,15 @@ app.registerExtension({
         console.log("MyComfyPlugin setup")
     },
     async beforeRegisterNodeDef(nodeType, nodeData, app) {
-        if(nodeData.name === "TextInput") {
+        if (nodeData.name === "TextInput") {
             const origOnNodeCreated = nodeType.prototype.onNodeCreated;
             nodeType.prototype.onNodeCreated = function () {
                 const r = origOnNodeCreated ? origOnNodeCreated.apply(this) : undefined;
-                for(const w of this.widgets) {
-                    if(w.name === "seed")
-                    {
+                for (const w of this.widgets) {
+                    if (w.name === "seed") {
                         w.type = "converted-widget";
-                        if(w.linkedWidgets) {
-                            for(const lw of w.linkedWidgets) {
+                        if (w.linkedWidgets) {
+                            for (const lw of w.linkedWidgets) {
                                 lw.type = "converted-widget";
                             }
                         }
