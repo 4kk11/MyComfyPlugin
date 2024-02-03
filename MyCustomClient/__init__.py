1db35d7db5828d9cd112c5ba1efc24dbbc0fab90
4kk11 <akiaki118row@yahoo.co.jp>
2024-02-03 23:53:24 +0900

    Minor fix

--- MyCustomClient/__init__.py
+++ MyCustomClient/__init__.py
@@ -1,4 +1,3 @@
-import os
 import server
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 from aiohttp import web
@@ -9,8 +8,6 @@ __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
 @server.PromptServer.instance.routes.post('/my_custom_client/update_text')
 async def update_text(request):
     data = await request.json()
-    text = data["text"]
-    
     server.PromptServer.instance.send_sync("update_text", data)
     server.PromptServer.instance.send_sync("run_workflow", {})
     return web.Response()
